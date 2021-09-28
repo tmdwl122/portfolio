f07cfd8a0abc8398a02e8a14e37a8c6ad9e7f052
tmdwl122 <85921913+tmdwl122@users.noreply.github.com>
2021-09-28 18:12:31 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -40,11 +40,11 @@ $('#nav li a').on('click focus', function(e){
     
     if (num<3) {
         $('#sect4').removeClass('on')
-        $('#sect4 .formbox').css({
+        $('#sect4 h3').css({
             transitionDelay:'0s'
         })
     } else {
-        $('#sect4 .formbox').css({
+        $('#sect4 h3').css({
             transitionDelay:'1s'
         })
         $('#sect4').addClass('on')
@@ -110,7 +110,7 @@ $(window).on('scroll', function(){
         $('#nav li').eq(2).addClass('on')
         $('#nav li').eq(2).siblings().removeClass('on')
         $('#sect4').removeClass('on')
-        $('#sect4 .formbox').css({
+        $('#sect4 h3').css({
             transitionDelay: '0s'
         })
         $("#sect3").addClass('on')
@@ -150,6 +150,17 @@ $('.open').on('click', function(){
 
 })
 
+// 햄버거 메뉴박스 닫기
+$('.openlist li a').on('click', function(e){
+    e.preventDefault()
+    $('.open').removeClass('on')
+    var num = $(this).parent().index()
+    let opensct =  $("section").eq(num).offset().left
+    $('html, body').animate({
+        scrollLeft:opensct
+    }, 800)
+})
+
 
 
 // 세번째 section
@@ -157,11 +168,10 @@ $('.portfolio a').on('click', function(e){
     e.preventDefault()
     $('.portfolio').addClass('on')
     $('.close').addClass('on')
-    var src = $(this).find('img').attr('src') 
+    var src1 = $(this).attr('data-src1') 
     
-      
-    
-    $('body').append('<div class="outlayer"><img src="" alt=""></div>')
+
+    $('body').append(`<div class="outlayer"><img src="${src1}" alt=""></div>`)
     $('.outlayer').css({
         position: 'fixed',
         top: 0,
@@ -171,7 +181,7 @@ $('.portfolio a').on('click', function(e){
         backgroundColor: 'rgba(0,0,0,0.9)',
         zIndex: 999,  
         transition: '0.8',
-        transitionDelay: '0.8'
+        transitionDelay: '0.8',
     })
     
     .append('<button class="close"><i class="far fa-times-circle"></i></button>')
@@ -183,7 +193,7 @@ $('.portfolio a').on('click', function(e){
         fontSize: '40px',
         color: '#999',
     })
-    $('.outlayer img').attr('src',src).css({ 
+    $('.outlayer img').css({ 
         position: 'absolute',
         top:'20%',
         left: '25%',
@@ -193,8 +203,6 @@ $('.portfolio a').on('click', function(e){
         })
  
 })
-
-
     
 $('body').on('click','.close',function(){
     $('.close').removeClass('on')
@@ -209,4 +217,42 @@ $('body').on('click','.outlayer',function(){
 
 
 
+
+// 4번째 구역
+
+//jQuery time
+//jQuery time
+
+//add '.ready' to form when user focuses on it
+$("#email").focus(function(){
+	$("#cuboid form").addClass("ready");
+})
+//remove '.ready' when user blus away but only if there is no content
+$("#email").blur(function(){
+	if($(this).val() == "")
+		$("#cuboid form").removeClass("ready");
+})
+
+//If the user is typing something make the arrow green/.active
+$("#email").keyup(function(){
+	//this adds .active class only if the input has some text
+	$(".submit-icon").toggleClass("active", $(this).val().length > 0);
+})
+
+//on form submit remove .ready and add .loading to the form
+$("#cuboid form").submit(function(){
+	$(this).removeClass("ready").addClass("loading");
+	//finish loading in 3s
+	setTimeout(complete, 3000);
+	//prevent default form submisson
+	return false;
+})
+function complete()
+{
+	$("#cuboid form").removeClass("loading").addClass("complete");
+}
+//reset/refresh functionality
+$(".reset-icon").click(function(){
+	$("#cuboid form").removeClass("complete");
+})
 
